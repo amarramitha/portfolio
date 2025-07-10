4c1af253ca14d03855230317bb849be244de6dde
Amarramitha <amarra.mitha@gmail.com>
2025-07-10 19:35:53 +0700

    Updates

--- _next/static/chunks/pages/components/Skills-4d10e3ae845ba502.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[321],{7404:(_,n,s)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Skills",function(){return s(1699)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[916,699,636,593,792],()=>n(7404)),_N_E=_.O()}]);
